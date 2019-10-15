da9a02027ab867d65885f1e73e664b92281bd8a9
Pouya Eghbali <persian.writer@gmail.com>
2019-10-15 11:03:03 +0200

    Added cli

--- parser.js
+++ parser.js
@@ -3,6 +3,7 @@ const { unflatten } = require('flat')
 const fs = require("fs");
 const ncsvLexer = require("./lexer.js");
 const ncsvModel = fs.readFileSync("./ncsv.beef", { encoding: "utf8" });
+const model = beef(ncsvModel);
 
 Object.prototype.unflatten = function () {
   return unflatten(this)
@@ -12,21 +13,21 @@ Array.prototype.zip = function (arr) {
   return this.map((item, index) => [item, arr[index]])
 }
 
-// DEBUG
-
-const source = fs.readFileSync("./test.csv", { encoding: "utf8" });
-
-const tokens = ncsvLexer(source);
-const model = beef(ncsvModel);
-
-const [success, result] = bean(model, tokens);
-
-if (success) {
-  const cst = result[0];
-  console.dir(cst, { depth: null });
-} else {
-  const firstUnmatched = result[0].name
-  const expecting = model.filter(m => m.left == firstUnmatched)
-  const encountered = result[1].name
-  console.error(`Expecting one of ${expecting.join(', ')} but encountered ${encountered}`)
+const parse = source => {
+  const tokens = ncsvLexer(source);
+  const [success, result] = bean(model, tokens);
+  if (success) {
+    const cst = result[0];
+    return cst.rows
+  } else {
+    const firstUnmatched = result[0].name
+    const expecting = model.filter(m => m.left == firstUnmatched).map(({ right }) => right)
+    const encountered = result[1].name
+    const ParsingError = `Expecting one of ${expecting.join(', ')} but encountered ${encountered}`
+    throw ParsingError
+  }
 }
+
+module.exports.parse = parse
+module.exports.model = model
+module.exports.lexer = ncsvLexer
